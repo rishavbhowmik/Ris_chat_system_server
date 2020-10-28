9f27853e479cf7c306a8df75d13de79fa4e47207
Rishav Bhowmik <tgs.rishav.bhowmik@gmail.com>
2020-10-28 17:07:31 +0530

    integration of chat_bk_driver

--- sock_routes/UserSession.js
+++ sock_routes/UserSession.js
@@ -1,5 +1,6 @@
 const JWT = require('jsonwebtoken')
 const private_config =  require('../../configs/private.json')
+const chat_driver = require('../../chat_system_bk_driver/index')
 
 class UserSession{
     #connected = true
@@ -12,9 +13,28 @@ class UserSession{
         const user = user_token; //TEMP //DESTORY THIS LATER, IF I DONT
         console.log(user);
         this.#_id  = user._id;
+        return this.get_user_id()
     }
     get_user_id(){
         return this.#_id
+    }
+    async send_msg(data){
+        const sender_id  = this.#_id
+        const reciver_id = data.reciver_id
+        const message = new chat_driver.MessageObject()
+        data.lines.forEach(line => {
+            message.add_line(line.header, line.data)
+        });
+        const message_id = await chat_driver.SendMessage.send_message(sender_id, reciver_id, message)
+        return message_id
+    }
+    async get_pleanty_bothways(){
+        if(typeof this.#_id != 'string') throw Error('User Undefined')
+        const messages = await chat_driver.GetMessage.get_messages_bothway_pleanty(this.#_id, 10)
+        return messages.map((message_row)=>{
+            message_row.data = new MessageObject(message_row.data)
+            return message_row
+        })
     }
 }
 
--- sock_routes/index.js
+++ sock_routes/index.js
@@ -1,12 +1,23 @@
+const message = require('../../chat_system_bk_driver/operations/message')
 const UserSession = require('./UserSession')
 
 const sock_routes = (socket) => {
     const user = new UserSession()
     socket.on('verify', (data)=>{
-        user.verify_user(data.user_token)
+        if(!user.verify_user(data.user_token)){
+            socket.close(); return;
+        }
         console.log(user)
+        socket.emit('ready', '')
     })
-    socket.on('send', (data)=>{
+    socket.on('send_msg', (data)=>{
+        if (typeof user.get_user_id() != 'string') return
+        user.send_msg(data)
+        .then((message_id)=>{
+            socket.emit('msg_sent', {message_id})
+        })
+    })
+    socket.on('get_pleanty_bothways', data =>{
         
     })
 
