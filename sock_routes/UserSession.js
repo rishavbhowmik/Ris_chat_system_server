50ab8682101522cc6d2b32199bfb89c852d899e5
Rishav Bhowmik <tgs.rishav.bhowmik@gmail.com>
2020-10-30 11:41:09 +0530

    new functions included

--- sock_routes/UserSession.js
+++ sock_routes/UserSession.js
@@ -28,11 +28,83 @@ class UserSession{
         const message_id = await chat_driver.SendMessage.send_message(sender_id, reciver_id, message)
         return message_id
     }
-    async get_pleanty_bothways(){
+    async get_messages_bothway_pleanty(limit=15){
         if(typeof this.#_id != 'string') throw Error('User Undefined')
-        const messages = await chat_driver.GetMessage.get_messages_bothway_pleanty(this.#_id, 10)
+        const messages = await chat_driver.GetMessage.get_messages_bothway_pleanty(this.#_id, limit)
         return messages.map((message_row)=>{
-            message_row.data = new MessageObject(message_row.data)
+            message_row.data = new chat_driver.MessageObject(message_row.data)
+            return message_row
+        })
+    }
+    async get_messages_bothway_time_after(time_in_sec){//rec
+        if(typeof this.#_id != 'string') throw Error('User Undefined')
+        const messages = await chat_driver.GetMessage.get_messages_bothway_time_after(this.#_id, time_in_sec)//rec
+        return messages.map((message_row)=>{
+            message_row.data = new chat_driver.MessageObject(message_row.data)
+            return message_row
+        })
+    }
+    async get_messages_bothway_post_id(id_onwards){//rec
+        if(typeof this.#_id != 'string') throw Error('User Undefined')
+        const messages = await chat_driver.GetMessage.get_messages_bothway_post_id(this.#_id, id_onwards)//rec
+        return messages.map((message_row)=>{
+            message_row.data = new chat_driver.MessageObject(message_row.data)
+            return message_row
+        })
+    }
+    async get_messages_bothway_range_id(id_onwards, id_untill){//rec
+        if(typeof this.#_id != 'string') throw Error('User Undefined')
+        const messages = await chat_driver.GetMessage.get_messages_bothway_range_id(this.#_id, id_onwards, id_untill)//rec
+        return messages.map((message_row)=>{
+            message_row.data = new chat_driver.MessageObject(message_row.data)
+            return message_row
+        })
+    }
+    async get_messages_bothway_pleanty_of_sender(peer_id, limit = 15){//rec
+        if(typeof this.#_id != 'string') throw Error('User Undefined')
+        const messages = await chat_driver.GetMessage.get_messages_bothway_pleanty_of_sender(this.#_id, peer_id, limit)//rec
+        return messages.map((message_row)=>{
+            message_row.data = new chat_driver.MessageObject(message_row.data)
+            return message_row
+        })
+    }
+    async get_messages_bothway_post_id_of_sender(peer_id, id_onwards){//rec
+        if(typeof this.#_id != 'string') throw Error('User Undefined')
+        const messages = await chat_driver.GetMessage.get_messages_bothway_post_id_of_sender(this.#_id, peer_id, id_onwards)//rec
+        return messages.map((message_row)=>{
+            message_row.data = new chat_driver.MessageObject(message_row.data)
+            return message_row
+        })
+    }
+    async get_messages_bothway_range_id_of_sender(peer_id, id_onwards, id_untill){//rec
+        if(typeof this.#_id != 'string') throw Error('User Undefined')
+        const messages = await chat_driver.GetMessage.get_messages_bothway_range_id_of_sender(this.#_id, peer_id, id_onwards, id_untill)//rec
+        return messages.map((message_row)=>{
+            message_row.data = new chat_driver.MessageObject(message_row.data)
+            return message_row
+        })
+    }
+    async get_messages_oneway_pleanty_of_sender(peer_id, limit = 15){//rec
+        if(typeof this.#_id != 'string') throw Error('User Undefined')
+        const messages = await chat_driver.GetMessage.get_messages_oneway_pleanty_of_sender(this.#_id, peer_id, limit)//rec
+        return messages.map((message_row)=>{
+            message_row.data = new chat_driver.MessageObject(message_row.data)
+            return message_row
+        })
+    }
+    async get_messages_oneway_post_id_of_sender(peer_id, id_onwards){//rec
+        if(typeof this.#_id != 'string') throw Error('User Undefined')
+        const messages = await chat_driver.GetMessage.get_messages_oneway_post_id_of_sender(this.#_id, peer_id, id_onwards)//rec
+        return messages.map((message_row)=>{
+            message_row.data = new chat_driver.MessageObject(message_row.data)
+            return message_row
+        })
+    }
+    async get_messages_oneway_range_id_of_sender(peer_id,  id_onwards, id_untill){//rec
+        if(typeof this.#_id != 'string') throw Error('User Undefined')
+        const messages = await chat_driver.GetMessage.get_messages_oneway_range_id_of_sender(this.#_id, peer_id, id_onwards, id_untill)//rec
+        return messages.map((message_row)=>{
+            message_row.data = new chat_driver.MessageObject(message_row.data)
             return message_row
         })
     }
--- sock_routes/index.js
+++ sock_routes/index.js
@@ -11,15 +11,134 @@ const sock_routes = (socket) => {
         socket.emit('ready', '')
     })
     socket.on('send_msg', (data)=>{
+        const req_id = data.req_id;
         if (typeof user.get_user_id() != 'string') return
         user.send_msg(data)
         .then((message_id)=>{
-            socket.emit('msg_sent', {message_id})
+            socket.emit('msg_sent', {req_id, message_id})
         })
     })
-    socket.on('get_pleanty_bothways', data =>{
-        
+    socket.on('get_messages_bothway_pleanty', data =>{
+        const req_id = data.req_id;
+        user.get_messages_bothway_pleanty()
+        .then(results => {
+            results.map((message_row)=>{
+                //message_row.data = new MessageObject(message_row.data)
+                message_row.data = message_row.data.get_all_lines()
+                return message_row
+            })
+            socket.emit('get_messages_bothway_pleanty', {req_id, results})
+        })
     })
+    socket.on('get_messages_bothway_time_after', data =>{//rec
+        const req_id = data.req_id;
+        user.get_messages_bothway_time_after(data.time_in_sec)//rec + argc
+        .then(results => {
+            results.map((message_row)=>{
+                //message_row.data = new MessageObject(message_row.data)
+                message_row.data = message_row.data.get_all_lines()
+                return message_row
+            })
+            socket.emit('get_messages_bothway_time_after', {req_id, results}) //rec
+        })
+    })
+    socket.on('get_messages_bothway_post_id', data =>{//rec
+        const req_id = data.req_id;
+        user.get_messages_bothway_post_id(data.id_onwards)//rec + argc
+        .then(results => {
+            results.map((message_row)=>{
+                //message_row.data = new MessageObject(message_row.data)
+                message_row.data = message_row.data.get_all_lines()
+                return message_row
+            })
+            socket.emit('get_messages_bothway_post_id', {req_id, results}) //rec
+        })
+    })
+    socket.on('get_messages_bothway_range_id', data =>{//rec
+        const req_id = data.req_id;
+        user.get_messages_bothway_range_id(data.id_onwards, data.id_untill)//rec + argc
+        .then(results => {
+            results.map((message_row)=>{
+                //message_row.data = new MessageObject(message_row.data)
+                message_row.data = message_row.data.get_all_lines()
+                return message_row
+            })
+            socket.emit('get_messages_bothway_range_id', {req_id, results}) //rec
+        })
+    })
+    socket.on('get_messages_bothway_pleanty_of_sender', data =>{//rec
+        const req_id = data.req_id;
+        user.get_messages_bothway_pleanty_of_sender(data.sender_id)//rec + argc
+        .then(results => {
+            results.map((message_row)=>{
+                //message_row.data = new MessageObject(message_row.data)
+                message_row.data = message_row.data.get_all_lines()
+                return message_row
+            })
+            socket.emit('get_messages_bothway_pleanty_of_sender', {req_id, results}) //rec
+        })
+    })
+    socket.on('get_messages_bothway_post_id_of_sender', data =>{//rec
+        const req_id = data.req_id;
+        user.get_messages_bothway_post_id_of_sender(data.sender_id, data.id_onwards )//rec + argc
+        .then(results => {
+            results.map((message_row)=>{
+                //message_row.data = new MessageObject(message_row.data)
+                message_row.data = message_row.data.get_all_lines()
+                return message_row
+            })
+            socket.emit('get_messages_bothway_post_id_of_sender', {req_id, results}) //rec
+        })
+    })
+    socket.on('get_messages_bothway_range_id_of_sender', data =>{//rec
+        const req_id = data.req_id;
+        user.get_messages_bothway_range_id_of_sender(data.sender_id, data.id_onwards, data.id_untill )//rec + argc
+        .then(results => {
+            results.map((message_row)=>{
+                //message_row.data = new MessageObject(message_row.data)
+                message_row.data = message_row.data.get_all_lines()
+                return message_row
+            })
+            socket.emit('get_messages_bothway_range_id_of_sender', {req_id, results}) //rec
+        })
+    })
+    socket.on('get_messages_oneway_pleanty_of_sender', data =>{//rec
+        const req_id = data.req_id;
+        user.get_messages_oneway_pleanty_of_sender(data.sender_id)//rec + argc
+        .then(results => {
+            results.map((message_row)=>{
+                //message_row.data = new MessageObject(message_row.data)
+                message_row.data = message_row.data.get_all_lines()
+                return message_row
+            })
+            socket.emit('get_messages_oneway_pleanty_of_sender', {req_id, results}) //rec
+        })
+    })
+    socket.on('get_messages_oneway_post_id_of_sender', data =>{//rec
+        const req_id = data.req_id;
+        user.get_messages_oneway_post_id_of_sender(data.sender_id, data.id_onwards )//rec + argc
+        .then(results => {
+            results.map((message_row)=>{
+                //message_row.data = new MessageObject(message_row.data)
+                message_row.data = message_row.data.get_all_lines()
+                return message_row
+            })
+            socket.emit('get_messages_oneway_post_id_of_sender', {req_id, results}) //rec
+        })
+    })
+    socket.on('get_messages_oneway_range_id_of_sender', data =>{//rec
+        const req_id = data.req_id;
+        user.get_messages_oneway_range_id_of_sender(data.sender_id, data.id_onwards, data.id_untill )//rec + argc
+        .then(results => {
+            results.map((message_row)=>{
+                //message_row.data = new MessageObject(message_row.data)
+                message_row.data = message_row.data.get_all_lines()
+                return message_row
+            })
+            socket.emit('get_messages_oneway_range_id_of_sender', {req_id, results}) //rec
+        })
+    })
+
 
     socket.emit('connected','')
 }
